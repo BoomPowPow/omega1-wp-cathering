9dc5643091fd01caf5022177f919fd4e67d25a6e
BoomPowPow <henry.valdez.gil@gmail.com>
2017-06-02 13:22:52 -0400

    Defining Panels

---
+++ app/Controllers/AdminController.php
@@ -0,0 +1,25 @@
+<?php namespace Omega1WPCathering\Controllers;
+
+use Herber\Framework\Models\Post;
+use Herbert\Framework\Http;
+
+class AdminController
+{
+	
+	public function showOrders()
+	{
+		echo 'ORDERS';
+	}
+
+
+	public function showRecipes()
+	{
+		echo 'RECIPES';
+	}
+
+
+	public function showItems()
+	{
+		echo 'ITEMS';
+	}
+}
--- app/panels.php
+++ app/panels.php
@@ -1,3 +1,48 @@
 <?php namespace Omega1WPCathering;
 
 /** @var \Herbert\Framework\Panel $panel */
+
+// Main Panel
+$panel->add([
+    'type'   => 'panel',
+    'as'     => 'mainPanel',
+    'title'  => 'Service traiteur',
+    'rename' => 'General',
+    'slug'   => 'Omega1WPCathering-index',
+    'order'  => '35.555',
+    'uses'   => function()
+    {
+        return 'Hello World';
+    }
+]);
+
+// Orders Sub-Panel
+$panel->add([
+    'type'   => 'sub-panel',
+    'parent' => 'mainPanel',
+    'as'     => 'orders',
+    'title'  => 'Mes commandes',
+    'slug'   => 'Omega1WPCathering-orders',
+    'uses'   => __NAMESPACE__ . '\Controllers\AdminController@showOrders'
+]);
+// Recipe Sub-Panel
+$panel->add([
+    'type'   => 'sub-panel',
+    'parent' => 'mainPanel',
+    'as'     => 'recipes',
+    'title'  => 'Mes recettes',
+    'slug'   => 'Omega1WPCathering-recipes',
+    'uses'   => __NAMESPACE__ . '\Controllers\AdminController@showRecipes'
+]);
+// Items Sub-Panel
+$panel->add([
+    'type'   => 'sub-panel',
+    'parent' => 'mainPanel',
+    'as'     => 'items',
+    'title'  => 'Liste des ingredients',
+    'slug'   => 'Omega1WPCathering-items',
+    'uses'   => __NAMESPACE__ . '\Controllers\AdminController@showItems'
+]);
+
+
+
